1e6dcc600b35efb4ee0c4ca3d059ca80a2e09916
JuanJo Ciarlante <jjo@google.com>
2009-11-12 13:21:48 +0100

    * openvpn.exe tests: also send *.dll to remote

--- run-udp6-1-rem.sh
+++ run-udp6-1-rem.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 : ${REM6?}
-rsync -Lva ${OPENVPN?} ../openvpn.key "[${REM6?}]":/tmp
+XTRAS=""
+case "${OPENVPN}" in *exe) XTRAS="$(dirname ${OPENVPN})/*.dll";; esac
+rsync -Lva ${OPENVPN?} ../openvpn.key $XTRAS "[${REM6?}]":/tmp
 openvpn_fname=${OPENVPN##*/}
 ssh -t ${REM6?} "sh -c '/bin/fuser -k /tmp/${openvpn_fname}*;/tmp/${openvpn_fname} --verb 5 --dev null --proto udp6 --port 5010 --secret /tmp/openvpn.key --float $@'"
 #ssh -t ${REM6?} "sudo sh -c '/bin/fuser -k /tmp/${openvpn_fname}*;/tmp/${openvpn_fname} --verb 5 --dev tun --proto udp6 --port 5010 --secret /tmp/openvpn.key --float --ifconfig 1.1.1.1 1.1.1.253 $@'"
