20e25703fad432f5dcb4d64a5bc5f9b7cd0d386e
JuanJo Ciarlante <jjo+ml@google.com>
2010-09-23 20:45:26 +0200

    add UDP4 loopback_multihome test

--- run-all.sh
+++ run-all.sh
@@ -33,52 +33,59 @@ test_set_cleanup "${SUDO} killall $OPENVPN"
 
 trap 'test_bg_cleanup;exit' 0 2 15
 
+STR_INIT_OK="Initialization Sequence Completed"
+
 test_define "UDP6 loopback$post"
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-udp6-0-loopback.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-udp6-0-loopback.sh $O_ARGS
 
 test_define "UDP6 loopback byname$post"
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-udp6-0-loopback-byname.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-udp6-0-loopback-byname.sh $O_ARGS
 
 test_define "UDP6 loopback4mapped$post"
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-udp6-0-loopback4mapped.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-udp6-0-loopback4mapped.sh $O_ARGS
 
 test_define "UDP6 loopback4native$post"
 test_bg_prev ${tdir?}/run-udp6-0-loopback_passive.sh
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-udp6-0-loopback4native.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-udp6-0-loopback4native.sh $O_ARGS
 
 if ping6 -c 1 "$REM6" >/dev/null 2>&1;then
   test_define "UDP6 remote$post"
   test_bg_prev ${tdir?}/run-udp6-1-rem.sh --ping-exit 60 $O_ARGS
-  test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-udp6-1-loc.sh --ping-exit 60 $O_ARGS
+  test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-udp6-1-loc.sh --ping-exit 60 $O_ARGS
 
   test_define "TCP6 remote$post"
   test_bg_prev ${tdir?}/run-tcp6-1-server.sh --ping-exit 60
-  test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-tcp6-1-client.sh --ping-exit 60 $O_ARGS
+  test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-tcp6-1-client.sh --ping-exit 60 $O_ARGS
 else
   notice "REM6=\"$REM6\" not reachable, skipping remote UDP6,TCP6 tests"
 fi
 test_define "TCP6 loopback$post"
 test_bg_prev ${tdir?}/run-tcp6-0-loopback-server.sh $O_ARGS
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-tcp6-0-loopback-client.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-tcp6-0-loopback-client.sh $O_ARGS
 
 test_define "TCP6 loopback byname$post"
 test_bg_prev ${tdir?}/run-tcp6-0-loopback-server-byname.sh $O_ARGS
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-tcp6-0-loopback-client-byname.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-tcp6-0-loopback-client-byname.sh $O_ARGS
 
 test_define "TCP6 loopback client4mapped$post"
 test_bg_prev ${tdir?}/run-tcp6-0-loopback-server.sh $O_ARGS
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-tcp6-0-loopback-client4mapped.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-tcp6-0-loopback-client4mapped.sh $O_ARGS
 
 test_define "TCP6 loopback client4native$post"
 test_bg_prev ${tdir?}/run-tcp6-0-loopback-server.sh $O_ARGS
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-tcp6-0-loopback-client4native.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-tcp6-0-loopback-client4native.sh $O_ARGS
 
 test_define "UDP4 loopback$post"
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-udp4-0-loopback.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-udp4-0-loopback.sh $O_ARGS
+
+test_define "UDP4 loopback_multihome$post"
+test_bg_prev ${tdir?}/run-udp4-0-loopback_listen_multihome.sh  $O_ARGS
+test_bg_egrep 30 "Peer Connection Initiated with.*via" ${tdir?}/run-udp4-0-loopback_connect_multihome.sh $O_ARGS
+
 
 test_define "TCP4 loopback$post"
 test_bg_prev ${tdir?}/run-tcp4-0-loopback-server.sh  $O_ARGS
-test_bg_egrep 30 "Initialization Sequence Completed" ${tdir?}/run-tcp4-0-loopback-client.sh $O_ARGS
+test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-tcp4-0-loopback-client.sh $O_ARGS
 
 }
 
