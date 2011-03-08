9df91dacbeeb64dbd1b149b170aac6bae4dcfccc
JuanJo Ciarlante <jjo@google.com>
2011-03-08 10:33:04 +0100

    added some gdb helpers

---
+++ gdb-1.sh
@@ -0,0 +1,1 @@
+./openvpn --dev null --proto tcp-client --remote localhost --rport 5011 --tls-client --ca easy-rsa/2.0/keys/ca.crt --cert easy-rsa/2.0/keys/client1.crt --key easy-rsa/2.0/keys/client1.key
