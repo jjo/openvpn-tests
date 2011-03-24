84977bc6a2c584695f582270e8a8555dc0824719
JuanJo Ciarlante <juanjosec@gmail.com>
2011-03-24 10:32:11 +0100

    run-all.sh cleanup

--- run-all.sh
+++ run-all.sh
@@ -24,13 +24,8 @@ else
 fi
 
 all_tests(){
-local post="$1"
-
 test_set_cleanup "pkill ${OPENVPN##*/};fuser -k $OPENVPN >/dev/null 2>&1"
-
 trap 'test_bg_cleanup;exit' 0 2 15
-
-STR_INIT_OK="Initialization Sequence Completed"
 
 test_cleanup
 for t in \
@@ -42,25 +37,9 @@ for t in \
 	plan="$(${tdir?}/$t plan)"
 	eval "$plan"
 done
-
-
-if _ping6 -c 1 "$REM6" >/dev/null 2>&1;then
-  test_define "UDP6 remote$post"
-  test_bg_prev ${tdir?}/run-udp6-1-rem.sh --ping-exit 60 $O_ARGS
-  test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-udp6-1-loc.sh --ping-exit 60 $O_ARGS
-
-  test_define "TCP6 remote$post"
-  test_bg_prev ${tdir?}/run-tcp6-1-server.sh --ping-exit 60
-  test_bg_egrep 30 "$STR_INIT_OK" ${tdir?}/run-tcp6-1-client.sh --ping-exit 60 $O_ARGS
-else
-  notice "REM6=\"$REM6\" not reachable, skipping remote UDP6,TCP6 tests"
-fi
-
 test_report
 
 }
 
 #export GDB="valgrind"
 all_tests
-export GDB="valgrind"
-#all_tests .vg
